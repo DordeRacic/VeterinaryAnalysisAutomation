1e428e4bc0318586a37d5acc0731295ee4932153
Đorđe Racić <djordjeracic3@gmail.com>
2025-12-09 09:50:36 -0600

    Clean up

--- scripts/maze_merging_final_files.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df_non_exp = pd.read_csv('merged_results_non_expert.csv')
-df_exp = pd.read_csv('merged_results_expert.csv')
-
-merged_final = pd.merge(df_exp, df_non_exp, on='Filename', how='inner')
-merged_final.to_excel('merged_final.xlsx')
